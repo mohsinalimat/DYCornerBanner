343e139e5282893666aa9599b7892f9a30b42a28
Dominik <dominikbutz@gmail.com>
2017-04-11 11:59:40 +0800

    Initial Commit

---
+++ DYCornerBanner/DYCornerBanner.h
@@ -0,0 +1,19 @@
+//
+//  DYCornerBanner.h
+//  DYCornerBanner
+//
+//  Created by Dominik Butz on 11.04.17.
+//  Copyright © 2017 Duoyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DYCornerBanner.
+FOUNDATION_EXPORT double DYCornerBannerVersionNumber;
+
+//! Project version string for DYCornerBanner.
+FOUNDATION_EXPORT const unsigned char DYCornerBannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DYCornerBanner/PublicHeader.h>
+
+
